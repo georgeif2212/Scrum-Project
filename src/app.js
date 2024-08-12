2f46cfec97ca2ead36a35cf893181c3152262a97
georgeif2212 <jinfante2212@gmail.com>
2024-08-12 14:57:35 -0600

    feat: incorporation of department routers

--- src/app.js
+++ src/app.js
@@ -1,29 +1,32 @@
-import express from 'express';
-import handlebars from 'express-handlebars';
-import path from 'path';
+import express from "express";
+import handlebars from "express-handlebars";
+import path from "path";
 
-import indexRouter from './routers/views/index.router.js';
-import productsRouter from './routers/api/products.router.js';
-import productsViewsRouter from './routers/views/products.router.js';
+import productsRouter from "./routers/api/products.router.js";
+import departmentsRouter from "./routers/api/departments.router.js";
 
-import { __dirname } from './utils.js';
+import indexRouter from "./routers/views/index.router.js";
+import productsViewsRouter from "./routers/views/products.router.js";
+import departmentsViewsRouter from "./routers/views/departments.router.js";
+
+import { __dirname } from "./utils.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../public')));
-app.engine('handlebars', handlebars.engine());
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, "../public")));
+app.engine("handlebars", handlebars.engine());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "handlebars");
 
-app.use('/', indexRouter,productsViewsRouter);
-app.use('/api', productsRouter);
+app.use("/", indexRouter, productsViewsRouter, departmentsViewsRouter);
+app.use("/api", productsRouter, departmentsRouter);
 
 app.use((error, req, res, next) => {
   const message = `Ah ocurrido un error desconocido 😨: ${error.message}`;
   console.log(message);
-  res.status(500).json({ status: 'error', message });
+  res.status(500).json({ status: "error", message });
 });
 
 export default app;
