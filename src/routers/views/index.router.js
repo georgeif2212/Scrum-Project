9495e464b49b59e4b783d16b8bd9ddf6dfc1389b
georgeif2212 <jinfante2212@gmail.com>
2024-08-12 14:26:47 -0600

    fix: the endpoints were renamed

--- src/routers/views/index.router.js
+++ src/routers/views/index.router.js
@@ -1,35 +1,18 @@
 import { Router } from "express";
-import ProductModel from '../../models/product.model.js';
+import ProductModel from "../../models/product.model.js";
 
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.render("index", { title: "Hello People 🖐️" });
+  res.redirect("/main");
 });
 
 router.get("/main", (req, res) => {
-  res.render("menu_main", { title: "Menu principal 🖐️" });
+  res.render("main", { title: "Menu principal 🖐️" });
 });
 
-router.get("/menu-products", (req, res) => {
-  res.render("menu_productos", { title: "Menu de productos 🖐️" });
-});
-
-router.get("/products", (req, res) => {
-  res.render("alta_producto", { title: "Alta de productos 🖐️" });
-});
-
-router.get("/products-delete", (req, res) => {
-  res.render("baja_producto", { title: "Baja de productos 🖐️" });
-});
-
-router.get("/products-list", async (req, res) => {
-  const products = await ProductModel.find({});
-  const plainProducts = products.map((product) => product.toObject()); // Convertir los
-  res.render("products_list", {
-    title: "Alta de productos 🖐️",
-    products: plainProducts,
-  });
+router.get("/products-main", (req, res) => {
+  res.render("products_menu", { title: "Menu de productos 🖐️" });
 });
 
 export default router;
