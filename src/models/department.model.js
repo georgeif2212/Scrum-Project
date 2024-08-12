fd7af0dff2491d12e49605d27c7ccc62dc19e587
georgeif2212 <jinfante2212@gmail.com>
2024-08-12 14:54:37 -0600

    fix: the department model didn't have the key department field

--- src/models/department.model.js
+++ src/models/department.model.js
@@ -2,6 +2,7 @@ import mongoose, { Schema } from 'mongoose';
 
 const departmentSchema = new Schema({
   name: { type: String, required: true },
+  keyDepartment: { type: String, required: true },
   personInCharge: { type: String, required: true },
 }, { timestamps: true });
 
