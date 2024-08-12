e9515466c031a3c86b81eee76e940d475fa5d46a
georgeif2212 <jinfante2212@gmail.com>
2024-08-12 14:38:46 -0600

    feat: new model to manage departmen data

---
+++ src/models/department.model.js
@@ -0,0 +1,8 @@
+import mongoose, { Schema } from 'mongoose';
+
+const departmentSchema = new Schema({
+  name: { type: String, required: true },
+  personInCharge: { type: String, required: true },
+}, { timestamps: true });
+
+export default mongoose.model('Product', departmentSchema);
