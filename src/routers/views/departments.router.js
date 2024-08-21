a7594b1804792dd4d36ddf188d47921e93a7a473
georgeif2212 <jinfante2212@gmail.com>
2024-08-20 21:37:04 -0600

    move: the departments views were moved to departmetns folder to maintain order

--- src/routers/views/departments.router.js
+++ src/routers/views/departments.router.js
@@ -7,18 +7,18 @@ const router = Router();
 router.get("/departments", async (req, res) => {
   const departments = await DepartmentModel.find({});
   const plainDepartments = departments.map((department) => department.toObject()); // Convertir los
-  res.render("departments_list", {
+  res.render("./departments/departments_list", {
     title: "Alta de departamentos 🖐️",
     departments: plainDepartments,
   });
 });
 
 router.get("/post/departments", (req, res) => {
-  res.render("post_department", { title: "Alta de departamentos 🖐️" });
+  res.render("./departments/post_department", { title: "Alta de departamentos 🖐️" });
 });
 
 router.get("/delete/departments", (req, res) => {
-  res.render("delete_department", { title: "Baja de departamentos 🖐️" });
+  res.render("./departments/delete_department", { title: "Baja de departamentos 🖐️" });
 });
 
 
