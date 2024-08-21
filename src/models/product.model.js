7a95fced2bdad7b9f668d2e75091a19f8539c6f5
georgeif2212 <jinfante2212@gmail.com>
2024-08-20 20:22:59 -0600

    feat: the keyDepartment and price was added to Product model

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -3,6 +3,8 @@ import mongoose, { Schema } from 'mongoose';
 const productSchema = new Schema({
   nombre: { type: String, required: true },
   proveedor: { type: String, required: true },
+  keyDepartment: { type: String, required: true, ref: 'Department' }, 
+  price: { type: Number, required: true } 
 }, { timestamps: true });
 
 export default mongoose.model('Product', productSchema);
