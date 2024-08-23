01594f60fe58615d4aaf747669589f83b7d761a2
georgeif2212 <jinfante2212@gmail.com>
2024-08-22 18:38:03 -0600

    fix: the keyDepartment and price don't must to be as required

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -3,8 +3,8 @@ import mongoose, { Schema } from 'mongoose';
 const productSchema = new Schema({
   nombre: { type: String, required: true },
   proveedor: { type: String, required: true },
-  keyDepartment: { type: String, required: true, ref: 'Department' }, 
-  price: { type: Number, required: true } 
+  keyDepartment: { type: String, required: false, ref: 'Department' }, 
+  price: { type: Number, required: false } 
 }, { timestamps: true });
 
 export default mongoose.model('Product', productSchema);
